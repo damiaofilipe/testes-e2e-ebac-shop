65313a407b77f2be65ef8fa538a824a8b11d4752
Damião Filipe <damiaofilipe@hotmail.com>
2021-10-25 22:20:52 -0300

    Primeiro comitt com as alterações

--- cypress/integration/e2e.spec.js
+++ cypress/integration/e2e.spec.js
@@ -1,5 +1,6 @@
 /// <reference types="cypress" />
 
+
 context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
     /*  Como cliente 
         Quero acessar a Loja EBAC 
@@ -10,12 +11,48 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
         E validando minha compra ao final */
 
     beforeEach(() => {
-        cy.visit('/')
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
     });
 
-    it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
-        //TODO 
+    it('Deve selecionar o primeiro produto da compra', () => {
+
+
+        cy.get('[class="product-block grid"]')
+        .contains('Apollo Running Short').click()
+        cy.get('.button-variable-item-32').click()
+        cy.get(':nth-child(2) > .value > .variable-items-wrapper > .variable-item').click()
+        cy.get('.input-text').clear().type(4)
+        cy.get('.single_add_to_cart_button').click()  
+
+        
+
+        cy.get('[class="product-block grid"]')
+        .contains('Arcadio Gym Short').click()
+        cy.get('.button-variable-item-36').click()
+        cy.get('.button-variable-item-Red').click()
+        cy.get('.input-text').clear().type(2)
+        cy.get('.single_add_to_cart_button').click()
+
+        cy.get('[class="product-block grid"]')
+        .contains('Arcadio Gym Short').click()
+        cy.get('.button-variable-item-32').click()
+        cy.get('.button-variable-item-Blue').click()
+        cy.get('.input-text').clear().type(1)
+        cy.get('.single_add_to_cart_button')
+
+        cy.get('[class="product-block grid"]')
+        .contains('Arcadio Gym Short').click()
+        cy.get('.button-variable-item-36').click()
+        cy.get('.button-variable-item-Blue').click()
+        cy.get('.input-text').clear().type(1)
+        cy.get('.single_add_to_cart_button')
+    
+     
+        cy.get('.dropdown-toggle > .mini-cart-items').should('contain', 8)
+       
+
+
     });
 
-
-})
+   
+}) 
